0e974f316bc6053a0f531fa1f86dd213c72fbf1f
Ubuntu <scom-sv-admin@React01.ex3uzpju1nyupeq0kn1pn4qc2d.lx.internal.cloudapp.net>
2024-04-28 09:17:25 +0000

    ad AI SDK2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 }
 
 const appInsights = new ApplicationInsights({ config: {
-  connectionString: 'InstrumentationKey=9a75d247-e558-463b-923c-950637fe5deb;IngestionEndpoint=https://japaneast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://japaneast.livediagnostics.monitor.azure.com/;ApplicationId=838bf4b3-a996-4b96-a0cc-3fe391df855f'
+  connectionString: 'InstrumentationKey=9a75d247-e558-463b-923c-950637fe5deb;IngestionEndpoint=https://japaneast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://japaneast.livediagnostics.monitor.azure.com/'
   /* ...Other Configuration Options... */
 } });
 appInsights.loadAppInsights();
